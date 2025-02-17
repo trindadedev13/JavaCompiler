419b625182b3aea939018f8b339019b7a9c023a0
trindadedev13 <trindadedev13@gmail.com>
2025-02-16 21:42:52 -0300

    fix: Bad formatting of printed result

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
   Lexer lexer(code);
   std::list<Token> tokens = lexer.tokenize();
   for (Token token : tokens) {
-    printf("Type = %s\nValue = %s", token.type_as_str().c_str(), token.value.c_str());
+    printf("Type: %s\nValue: %s\n\n", token.type_as_str().c_str(), token.value.c_str());
   }
   return 0;
 }
